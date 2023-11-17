e7e236088c0d3d4a7d9bf83de74f84224a282f7b
etud user <etud@port-hp-probook650G8-1>
2023-11-17 11:39:19 +0100

    javadoc

---
+++ src/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Batiment"},{"p":"<Unnamed>","l":"Quartier"},{"p":"<Unnamed>","l":"TestBatiment"}];updateSearchResults();
